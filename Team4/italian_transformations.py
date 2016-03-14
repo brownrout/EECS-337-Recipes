953e91ca14260be3bc3fc7054d8bb70c0be7f92c
Gates Porter <gporte28@dhcp-10-105-13-139.wireless.northwestern.private>
2016-03-14 16:38:58 -0500

    added italian transformations list

--- Team4/italian_transformations.py
+++ Team4/italian_transformations.py
@@ -1,137 +1,111 @@
 spicy_list = {
-    'jalapeno' : ['red chili pepper', 'green chili pepper', 'red chili pepper'],
-    'wasabi': ['red chili pepper', 'green chili pepper', 'chili powder'],
-    'tobasco': ['red chili pepper', 'green chili pepper', 'chili powder'],
-    'hot sauce': ['red chili pepper', 'green chili pepper', 'chili powder'],
-    'habanero pepper' : ['red chili pepper', 'green chili pepper', 'red chili pepper'],
+    'jalapeno' : ['black pepper', 'chili flakes', 'black pepper'],
+    'wasabi': ['black pepper', 'chili flakes', 'black pepper'],
+    'tobasco': ['black pepper', 'chili flakes', 'black pepper'],
+    'hot sauce': ['black pepper', 'chili flakes', 'black pepper'],
+    'habanero pepper' : ['black pepper', 'chili flakes', 'black pepper'],
 
 }
 
-spicy_stopwords = ['peppers', 'pepper']
+spicy_stopwords = ['pepper', 'flakes']
 
-indian_spices = [
-    'turmeric',
-    'corriander',
-    'cumin',
-    'curry powder',
-    'chaat masala',
-    'garam masala',
-    'chili powder',
-    'saffron',
-    'cardamon',
-    'clove',
-    'mustard seeds',
-    'fenugreek seeds',
-    'carom seeds',
-    'fennel seeds',
-    'ginger',
+italian_spices = [
+    'rosemary',
+    'thyme',
+    'oregano',
+    'marjoram',
+    'basil',
+    'bay leaf',
+    'sage',
     'cilantro',
-    'black salt',
-    'mace',
-    'tulsi herb'
-
+    'red pepper flakes',
+    'savory',
+    'garlic powder',
+    'anise',
+    'chamomile',
+    'crushed red pepper',
+    'garlic',
+    'parsley',
+    'sun-dried tomatoes'
 ]
 
 cheeses = {
-    'mozzerella': 'paneer',
-    'cheddar' : 'paneer',
-    'anejo enchilado' :'paneer',
-    'asiago':'paneer',
-    'blue': 'paneer',
-    'cotija' : 'paneer',
-    'gruyere' : 'paneer',
-    'parmesan': 'paneer',
-    'romano' : 'paneer',
-    'gouda' : 'paneer',
-    'raclette': 'paneer',
-    'swiss': 'paneer',
-    'cheddar' : 'paneer',
-    'chevres' : 'paneer',
-    'edam' : 'paneer',
-    'monterey jack': 'paneer',
-    'provolone' : 'paneer',
-    'butterkase' : 'paneer',
-    'colby': 'paneer',
-    'fontina val daosta':'paneer',
-    'havarti': 'paneer',
-    'brie' : 'paneer',
-    'camembert de normandie' :'paneer',
-    'gorgonzola' : 'paneer',
-    'limburger' : 'paneer',
-    'feta': 'paneer',
-    'munster' : 'paneer',
-    'neufchatel' : 'paneer',
-    'queso' : 'paneer',
-    'burrata' : 'paneer',
-    'curd' : 'paneer',
-    'chechil' : 'paneer',
-    'chura kampo' : 'paneer',
-    'american cheese' : 'paneer',
-    'roquefort': 'paneer',
-    'camembert': 'paneer',
-    'cotija': 'paneer',
-    'chevre': 'paneer',
-    'emmental': 'paneer',
-    'taleggio': 'paneer',
-    'parmigiano-reggiano': 'paneer',
-    'manchego': 'paneer',
-    'cream cheese' : 'yogurt'
+    'cheddar' : 'gorgonzola',
+    'anejo enchilado' :'gorgonzola',
+    'blue': 'robiola piemonte',
+    'cotija' : 'mozzarella',
+    'gruyere' : 'pecorino toscano',
+    'romano' : 'pecorino toscano',
+    'gouda' : 'robiola piemonte',
+    'raclette': 'asiago',
+    'swiss': 'asiago',
+    'cheddar' : 'mozzarella',
+    'chevres' : 'robiola piemonte',
+    'edam' : 'parmigiano-reggiano',
+    'monterey jack': 'parmigiano-reggiano',
+    'butterkase' : 'mozzarella',
+    'colby': 'asiago',
+    'havarti': 'mozzarella',
+    'brie' : 'gorgonzola',
+    'camembert de normandie' :'robiola piemonte',
+    'limburger' : 'asiago',
+    'feta': 'pecorino toscano',
+    'munster' : 'gorgonzola',
+    'neufchatel' : 'ricotta',
+    'queso' : 'mozzarella',
+    'curd' : 'burrata',
+    'chechil' : 'gorgonzola',
+    'chura kampo' : 'ricotta',
+    'american cheese' : 'mozzarella',
+    'roquefort': 'piave',
+    'camembert': 'mozzarella',
+    'cotija': 'asiago',
+    'chevre': 'parmigiano-reggiano',
+    'emmental': 'asiago',
+    'manchego': 'piave',
+    'cream cheese' : 'mascarpone'
 
 
 }
 
 sauce_list = {
-    'yogurt' : 'dahi',
-    'gravy' : 'creamy spiced gravy'
+    'yogurt' : 'italian yogurt',
+    'gravy' : 'italian gravy'
 }
 
 indian_sauces = [
-    'coriander chutney',
-    'mint chutney',
-    'tomato puree',
-    'tamarind chutney',
-    'raita',
-    'kuzhambu',
-    'rasam',
-    'sambar',
-    'onion-tomato based gravy'
+    'marinara sauce',
+    'neapolitan ragù',
+    'pesto',
+    'ragu',
+    'vodka sauce',
+    'peara',
+    'genovese sauce',
+    'gremolada',
+    'arrabbiata sauce'
 ]
 
 
 
 meats = {
-    'steak': ['lamb kabob', 'chicken'],
-    'beef': ['lamb kabob', 'chicken'],
-    'tuna' : ['prawn', 'crab'],
-    'anchovies' : ['prawn', 'black salt'],
-    'pork' : ['chicken', 'lamb kabob'],
-    'eel' : ['chicken', 'lamb kabob'],
-    'tofu' : ['paneer', 'paneer'],
-    'bacon' : ['chicken', 'chicken']
+    'steak': ['coppa', 'chicken'],
+    'beef': ['coppa', 'chicken'],
+    'tuna' : ['shrimp', 'crab'],
+    'pork' : ['chicken', 'coppa'],
+    'eel' : ['chicken', 'flounder'],
+    'tofu' : ['italian tofu', 'italian tofu'],
+    'bacon' : ['speck alto adige', 'chicken']
 
 }
 
 vegetables_list = {
-    'okra' : 'bhindi',
-    'asparagus' : 'bhindi',
-    'squash' : 'aloo',
-    'corn' : 'capsicum',
-    'refried beans' : 'rajma',
-    'chick peas' : 'chole',
-    'butternut squash' : 'eggplant',
-    'lettuce' : 'cabbage',
-    'broccoli' : 'cauliflower',
-    'blueberries' : 'pommegranate',
-    'watermelon' : 'mango',
-    'arugula' : 'spinach',
-    'olives' : 'mushrooms',
-    'zucchini' : 'cucumber',
-    'spaghetti squash' : 'green beans',
-    'noodles' : 'rice',
-    'penne pasta' : 'rice',
-    'spaghetti' :'rice',
-    'farfalle pasta': 'rice',
-    'pasta' :'rice',
+    'squash' : 'zucca',
+    'refried beans' : 'fava beans',
+    'butternut squash' : 'zucca',
+    'lettuce' : 'radicchio',
+    'olives' : 'castelvetrano olives',
+    'spaghetti squash' : 'zucca',
+    'noodles' : 'spaghetti'
 
 }
 
